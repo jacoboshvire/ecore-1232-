333813e2496806eb8f2227097e42403d8487f3fa
jacoshevire <ejirojacob2000@gmail.com>
2024-10-05 07:41:52 +0100

    commit@ 7:38 10524

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
-
+import Image from "next/image";
+import Wave from './image/Wave.png';
+import Profile from './image/Group_4.png';
 import "./page.css";
 import Nav from './nav.js'
 import Footer from './Footer.js'
