71ebd95123c3d45fc26cb3fb310996f3cc53589e
jacoshevire <ejirojacob2000@gmail.com>
2025-01-09 12:57:43 +0100

    commited@12:57 1925

--- app/About/image2.js
+++ app/About/image2.js
@@ -5,9 +5,9 @@ import P4 from '../image/imagep4.jpg'
 import P5 from '../image/imagep5.png'
 
 export const image = [
-    { src: P1, alt: "p1", id: 1 },
-    { src: P2, alt: "p2", id: 2 },
-    { src: P3, alt: "p3", id: 3 },
-    { src: P4, alt: "p4", id: 4 },
-    { src: P5, alt: "p5", id: 5 },
+    { src: P1, alt: "p1", id: 1, message: "Just my leg in picture. Just saying" },
+    { src: P2, alt: "p2", id: 2, message: "my face. side profile 👌😁" },
+    { src: P3, alt: "p3", id: 3, message: "my face. front view still perfect 😁👌👍" },
+    { src: P4, alt: "p4", id: 4, message: "ok" },
+    { src: P5, alt: "p5", id: 5, message: "tooo good" },
 ]
