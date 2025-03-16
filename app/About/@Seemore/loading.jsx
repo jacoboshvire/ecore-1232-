2e045fc59d8406ac66ccaa9ccbb0bf3de7a36ca6
jacoshevire <ejirojacob2000@gmail.com>
2025-03-16 21:00:25 +0100

    commited@21:00 31625

--- app/About/@Seemore/loading.jsx
+++ app/About/@Seemore/loading.jsx
@@ -1,7 +1,56 @@
+import './loading2.css'
+import "../about.css"
+
 export default function Loadingabt() {
     return (
-        <div>
+        <div className='seemore'>
+            <div className="profilePic1">
+                <div className="imageAbt">
 
+                </div>
+                <p>
+                </p>
+            </div>
+
+            <div className="writeupAbtsm1">
+                <div className="insidewriteupAbt">
+                    <p className='stTitleAbt1'>
+
+                    </p>
+                    <br />
+                    <p className='stTitleAbt1'>
+
+                    </p>
+                    <br />
+                    <p className='stTitleAbt1'>
+
+                    </p>
+                    <br />
+                    <p className='stTitleAbt1'>
+
+                    </p>
+                    <br />
+                    <p className='stTitleAbt1'>
+
+                    </p>
+                    <br />
+                    <p className='stTitleAbt1'>
+
+                    </p>
+                    <br />
+                    <p className='stTitleAbt1'>
+
+                    </p>
+                </div>
+                <div className="socialmediaAbt1">
+                    <div className="titleSMAbt1">
+                        <h2>
+
+                        </h2>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
